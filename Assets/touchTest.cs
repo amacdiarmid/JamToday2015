a9f7385ef8923969f8f7fc3032c097fec7a97fc8
amacdiarmid <amacdiarmid@hotmail.co.uk>
2015-09-11 22:31:01 +0100

    unity folders etc

---
+++ Assets/touchTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class touchTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Touch myTouch = Input.GetTouch(0);
+
+        //Touch[] myTouches = Input.touches;
+        //for (int i = 0; i < Input.touchCount; i++)
+       // {
+       //     Debug.Log("touch");
+        //}
+        transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+    }
+}
