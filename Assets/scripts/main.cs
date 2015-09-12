54f7479eca3e9cd92304717c11d21d317719d050
craigaubrey <c.aubrey@btinternet.com>
2015-09-12 01:16:02 +0100

    menu and credits

---
+++ Assets/scripts/main.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeScene (string sceneName){
+		Application.LoadLevel (sceneName);
+	}
+
+}
